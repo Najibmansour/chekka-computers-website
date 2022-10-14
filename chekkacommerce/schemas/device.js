ad33bc6f95f9bafd5b3f2fa28e359a69257ed871
najibmansour <najibmansour2005@gmail.com>
2022-10-14 23:20:03 +0300

    added repair service

--- chekkacommerce/schemas/device.js
+++ chekkacommerce/schemas/device.js
@@ -1,47 +1,52 @@
-    export default {
-    name: 'device',
-    title: 'Device',
-    type: 'document',
-    fields: [
-      {
-        name: "docid",
-        title: "ID",
-        readOnly: true,
-        type: "string",
-        initialValue: () => {
-          let d = new Date();
-          let str = d.getTime().toString();
-          return str.slice(4,10)
-
-        }},
-       { 
-        name: 'deviceName',
-        title: 'DeviceName',
-        type: 'string',
-        },
-        { 
-        name: 'ownerName',
-        title: 'OwnerName',
-        type: 'string',
-        },
-       { 
-        name: 'contact',
-        title: 'Contact',
-        type: 'string',
-        },
-        
-      {
-        name: "date",
-        title: "Date added",
-        type: "string",
-        readOnly: true,
-        initialValue: () => {
-          let d = new Date();
-          return `${d.getFullYear()}-${d.getMonth()+1}-${d.getDate()} ${d.getHours()}:${d.getMinutes()}`
-
-        }
-      } 
-   
-   ]
-    }
-
+export default {
+  name: "device",
+  title: "Device",
+  type: "document",
+  fields: [
+    {
+      name: "docid",
+      title: "ID",
+      readOnly: true,
+      type: "string",
+      initialValue: () => {
+        let d = new Date();
+        let str = d.getTime().toString();
+        return str.slice(4, 10);
+      },
+    },
+    {
+      name: "deviceName",
+      title: "DeviceName",
+      type: "string",
+    },
+    {
+      name: "ownerName",
+      title: "OwnerName",
+      type: "string",
+    },
+    {
+      name: "contact",
+      title: "Contact",
+      type: "string",
+    },
+    {
+      name: "status",
+      title: "Status",
+      type: "boolean",
+      hidden: false,
+      initialValue: false,
+    },
+    {
+      name: "date",
+      title: "Date added",
+      type: "string",
+      readOnly: true,
+      initialValue: () => {
+        let d = new Date();
+        return `${d.getFullYear()}-${
+          d.getMonth() + 1
+        }-${d.getDate()} ${d.getHours()}:${d.getMinutes()}`;
+      },
+    },
+  ],
+};
--- pages/repair.js
+++ pages/repair.js
@@ -6,11 +6,42 @@ import Head from "next/head";
 const Repair = ({ products }) => {
   const ids = products.length;
   const IDS = products.map((product) => product.docid);
+  // const statuses =
+
+  const [searchTerm, setsearchTerm] = useState("");
 
   return (
     <div>
-      <div>
-        <input className="ass" type="text" placeholder="Search" />
+      <div className="flex justify-center m-9">
+        <input
+          className=" text-lg lg:px-2 lg:py-1 px-2 py-1   border-2 rounded-lg border-gray-600"
+          type="text"
+          placeholder="Search Number"
+          onChange={(event) => {
+            setsearchTerm(event.target.value);
+          }}
+        />
+      </div>
+
+      <div className="grid ls:m-9 m-6 text-md lg:grid-cols-4 grid-cols-2 lg:gap-8 gap-4">
+        {IDS.filter((val) => {
+          if (searchTerm == "") {
+            return val;
+          } else if (val.includes(searchTerm)) {
+            return val;
+          }
+        }).map((ID, key) => (
+          <div className="lg:p-8 px-3 py-2 border-2 m-36` border-gray-200 rounded-lg inline-flex justify-between max-w-sm">
+            <p>{ID}</p>
+            <div>
+              {products[key].status == true ? (
+                <p className="text-green-700 font-bold">Done</p>
+              ) : (
+                <p className="text-red-700 font-bold">Not done</p>
+              )}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
@@ -19,7 +50,7 @@ const Repair = ({ products }) => {
 export const getServerSideProps = async () => {
   const query = '*[_type == "device"]';
   const products = await client.fetch(query);
-
+  console.log(products);
   return {
     props: { products },
   };
